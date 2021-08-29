73055d321ee240d0dad1ffd9698f6d9901016595
MorseLeMonke <mpmcclennan@gmail.com>
2021-08-29 13:37:49 +1200

    Build 1.2 Splash Screen Not Working

--- app/src/main/java/com/example/anewhue/MainActivity.java
+++ app/src/main/java/com/example/anewhue/MainActivity.java
@@ -4,94 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Handler;
 import android.view.View;
 import android.widget.SeekBar;
 import android.widget.ToggleButton;
 import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
-    private SharedMemory mSharedMemory;
-    private ToggleButton mToggleButton;
-    private CountDownTimer mCountDownTimer;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
 
-        final SeekBar alpha, red, green, blue;
-        alpha = findViewById(R.id.seek_alpha);
-        red = findViewById(R.id.seek_red);
-        green = findViewById(R.id.seek_green);
-        blue = findViewById(R.id.seek_blue);
+        getSupportActionBar().hide();
 
-        mToggleButton = findViewById(R.id.startButton);
-
-        mSharedMemory = new SharedMemory(this);
-
-        SeekBar.OnSeekBarChangeListener changeListener = new SeekBar.OnSeekBarChangeListener() {
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                mSharedMemory.setAlpha(alpha.getProgress());
-                mSharedMemory.setRed(red.getProgress());
-                mSharedMemory.setGreen(green.getProgress());
-                mSharedMemory.setBlue(blue.getProgress());
-
-                if (ANewHueFilterService.STATE == ANewHueFilterService.STATE_ACTIVE) {
-                    Intent intent =new Intent(MainActivity.this, ANewHueFilterService.class);
-                    startService(intent);
-                }
-
-                mToggleButton.setChecked(ANewHueFilterService.STATE == ANewHueFilterService.STATE_ACTIVE);
-
+            public void run() {
+                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
             }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        };
-
-        alpha.setOnSeekBarChangeListener(changeListener);
-        red.setOnSeekBarChangeListener(changeListener);
-        green.setOnSeekBarChangeListener(changeListener);
-        blue.setOnSeekBarChangeListener(changeListener);
-
-        mToggleButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i =new Intent(MainActivity.this, ANewHueFilterService.class);
-                if (ANewHueFilterService.STATE == ANewHueFilterService.STATE_ACTIVE) {
-                    stopService(i);
-                } else {
-                    startService(i);
-                }
-                refresh();
-            }
-        });
-    }
-
-    private void refresh() {
-        if(mCountDownTimer != null)
-            mCountDownTimer.cancel();
-
-        mCountDownTimer = new CountDownTimer(100, 100) {
-            @Override
-            public void onTick(long l) {
-
-            }
-
-            @Override
-            public void onFinish() {
-                mToggleButton.setChecked(ANewHueFilterService.STATE == ANewHueFilterService.STATE_ACTIVE);
-            }
-        };
-
-        mCountDownTimer.start();
+        },3000);
     }
 }
